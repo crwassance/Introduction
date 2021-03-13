3fc1c6a0d0859f8af8385fb7fd7bde288efa47cf
Developpeur Account <myadressemailisthat@gmail.com>
2021-03-13 11:54:25 +0100

    Basics

---
+++ introduction.swift
@@ -0,0 +1,19 @@
+var Hello = "Hello World"
+print ("Hello World")
+
+let siteName:String = "Apple.com"
+print(siteName)
+
+let binaryNumber = 0b11111111
+print(binaryNumber)
+print(1231)
+
+let someCharacter:Character = "C"
+let someString:String = "Swift is awesome"
+
+let someFloat = 12.23
+let someAnotherFloat = 3.14e2 
+print(someFloat)
+print(someAnotherFloat)
+
+let result:Bool = false
